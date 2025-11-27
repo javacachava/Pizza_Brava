98bfdaa7d1555f1f7782ba6aa2b1e459e80020dc
javacachava <oliascencio20060806@gmail.com>
2025-11-27 17:09:57 -0600

    carga ni idea

--- src/constants/productConfig.js
+++ src/constants/productConfig.js
@@ -1,4 +1,17 @@
-export const INGREDIENTS_LIST = [];
-export const SIDES_LIST = [];
-export const DRINKS_LIST = [];
-export const CONFIG_RULES = {};
+export const FALLBACK_CONFIG = {
+  ingredients: [], 
+  sides: [],       
+  drinks: [],     
+  rules: {
+    ingredientPrice: 0,
+    sizes: {
+      Personal: { label: "Personal", priceModifier: 0 },
+      Grande: { label: "Gigante", priceModifier: 0 }
+    }
+  }
+};
+
+export const INGREDIENTS_LIST = FALLBACK_CONFIG.ingredients;
+export const SIDES_LIST = FALLBACK_CONFIG.sides;
+export const DRINKS_LIST = FALLBACK_CONFIG.drinks;
+export const CONFIG_RULES = FALLBACK_CONFIG.rules;
